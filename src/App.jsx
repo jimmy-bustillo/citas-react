e5363771c0df763a3a11ea049d01763568456e07
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-04-12 10:38:39 -0600

    Agrupa el Formulario y el Listado de Pacientes para usar Flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import Formulario from './components/Formulario'
-import Hedaer from './components/Header'
-import ListadoPacientes from './components/ListadoPacientes'
-
+import Formulario from "./components/Formulario";
+import Hedaer from "./components/Header";
+import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
- 
   return (
-    <div className='container mx-auto mt-20'>
-      <Hedaer/>
-      <Formulario/>
-      <ListadoPacientes/>
+    <div className="container mx-auto mt-20">
+      <Hedaer />
+      <div className="mt-12 md:flex">
+        <Formulario />
+        <ListadoPacientes />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
