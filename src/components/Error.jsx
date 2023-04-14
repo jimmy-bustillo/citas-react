27f1b8e140a9063707fb6b4934b78b755e6b4b2f
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-04-13 21:49:52 -0600

    Crea componente para mostrar errores

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+const Error = ({children}) => {
+   return  (
+        <div className="bg-red-800 text-white text-center p-3 mb-5 font-bold uppercase rounded-md">
+          {children}
+        </div>
+      )
+}
+
+export default Error
+
