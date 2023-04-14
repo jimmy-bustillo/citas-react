9caeec4ff1364db155b2f7e2bf7f8a57d6b8e3b7
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-04-14 13:01:31 -0600

    Muestra los valores de paciente utilizando destructuring

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,29 +1,30 @@
-const Paciente = () => {
+const Paciente = ({paciente}) => {
+  const { nombre, propietario, email, fecha, sintomas } = paciente 
   return (
-    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
         <p className="font-bold mb-3 text-gray-700 uppercase">
           Nombre: {""}
-          <span className="font-normal normal-case">Hook</span>
+          <span className="font-normal normal-case">{nombre}</span>
         </p>
 
         <p className="font-bold mb-3 text-gray-700 uppercase">
           Propietario: {""}
-          <span className="font-normal normal-case">Jimmy</span>
+          <span className="font-normal normal-case">{propietario}</span>
         </p>
 
         <p className="font-bold mb-3 text-gray-700 uppercase">
           Email: {""}
-          <span className="font-normal normal-case">correo@correo.com</span>
+          <span className="font-normal normal-case">{email}</span>
         </p>
 
         <p className="font-bold mb-3 text-gray-700 uppercase">
           Fecha Alta: {""}
-          <span className="font-normal normal-case">10 de Diciembre 2023</span>
+          <span className="font-normal normal-case">{fecha}</span>
         </p>
 
         <p className="font-bold mb-3 text-gray-700 uppercase">
           Sintomas: {""}
-          <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipiscing elit non eget, erat facilisi viverra sagittis integer leo urna nibh consequat, donec facilisis semper habitasse molestie diam iaculis sollicitudin posuere, congue nam himenaeos placerat dictum vitae lectus nunc.</span>
+          <span className="font-normal normal-case">{sintomas}</span>
         </p>
 
       </div>
