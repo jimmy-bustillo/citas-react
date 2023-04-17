81fed6e491a276b7ed96192ae56266a32c4fc5a0
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-04-16 23:55:40 -0600

    Agrega botón para eliminar paciente

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,5 +1,14 @@
-const Paciente = ({ paciente, setPaciente }) => {
-  const { nombre, propietario, email, fecha, sintomas } = paciente;
+const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
+  const { nombre, propietario, email, fecha, sintomas, id } = paciente;
+
+  const handleEliminar = () => {
+    const respuesta = confirm('Deseas eliminar este paciente?')
+
+    if(respuesta){
+      eliminarPaciente(id)
+    }
+  }
+
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">
@@ -33,7 +42,13 @@ const Paciente = ({ paciente, setPaciente }) => {
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-md"
           onClick={() => setPaciente(paciente)}
         >Editar</button>
-        
+        <button 
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white
+           font-bold rounded-md"
+           onClick={handleEliminar}
+        >Eliminar</button>
+
       </div>
     </div>
   );
