b27fa090882c0ef0daab235f9aef74d9061b347e
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-04-17 00:37:35 -0600

    Añade Local Storage para la persistencia de datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import Hedaer from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
 
-  const [pacientes, setPacientes] = useState([])
-  const [paciente, setPaciente] = useState({})
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem(`pacientes`)) ?? [];
+      setPacientes(pacientesLS)
+    };
+    obtenerLS()
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(`pacientes`, JSON.stringify(pacientes));
+  }, [pacientes]);
 
   const eliminarPaciente = (id) => {
-    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
+    const pacientesActualizados = pacientes.filter(
+      (paciente) => paciente.id !== id
+    );
 
     setPacientes(pacientesActualizados);
-  }
+  };
 
   return (
     <div className="container mx-auto mt-20">
       <Hedaer />
       <div className="mt-12 md:flex">
-        <Formulario 
+        <Formulario
           pacientes={pacientes}
           setPacientes={setPacientes}
           paciente={paciente}
           setPaciente={setPaciente}
         />
-        <ListadoPacientes 
+        <ListadoPacientes
           pacientes={pacientes}
           setPaciente={setPaciente}
           eliminarPaciente={eliminarPaciente}
